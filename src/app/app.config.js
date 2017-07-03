c21b3cc2e3ce512a626f0464370efc7bfc484772
Osmar Costa <osmar.costa.s@gmail.com>
2017-07-03 00:33:50 -0300

    Add module myApp to angularjs, add config to myApp and some states(home,products)

---
+++ src/app/app.config.js
@@ -0,0 +1,50 @@
+(function () {
+  angular.module('myApp')
+    .config(MyConfig);
+
+    MyConfig.$inject = [
+      'CONFIG',
+      '$urlRouterProvider',
+      '$stateProvider'
+    ];
+
+    function MyConfig(CONFIG, $urlRouterProvider, $stateProvider) {
+
+      $stateProvider.state('site', {
+        abstract: true,
+        templateUrl: CONFIG.app + 'template/index.html',
+        controller: 'TemplateController',
+        controllerAs: 'vm',
+        resolve: {
+          loadCtrl: ['$ocLazyLoad', function ($ocLazyLoad) {
+            return $ocLazyLoad.load([CONFIG.app + 'template/template.controller.js'])
+          }]
+        }
+      })
+      .state('site.home', {
+        url: '/',
+        templateUrl: CONFIG.app + 'home/index.html',
+        controller: 'HomeController',
+        controllerAs: 'vmHome',
+        resolve: {
+          loadCtrl: ['$ocLazyLoad', function ($ocLazyLoad) {
+            return $ocLazyLoad.load([CONFIG.app + 'home/home.controller.js'])
+          }]
+        }
+      })
+      .state('site.products', {
+        url: '/products',
+        templateUrl: CONFIG.app + 'products/index.html',
+        controller: 'ProductsController',
+        controllerAs: 'vmHome',
+        resolve: {
+          loadCtrl: ['$ocLazyLoad', function ($ocLazyLoad) {
+            return $ocLazyLoad.load([CONFIG.app + 'products/products.controller.js'])
+          }]
+        }
+      })
+      ;
+      $urlRouterProvider.otherwise('/');
+    }
+
+})();
---
+++ src/app/app.module.js
@@ -0,0 +1,31 @@
+(function () {
+  angular.module('myApp',[
+    'ui.router',
+    'oc.lazyLoad'
+  ])
+  .constant('CONFIG', {
+    'app': './app/',
+    'api': 'api/'
+  })
+  .run(runApp);
+
+  runApp.$inject = [
+    '$rootScope',
+    '$state'
+  ];
+  function runApp($rootScope, $state) {
+    $rootScope.$on('$stateChangeStart',
+      function (event, toState, toParams, fromState, fromParam) {
+    });
+
+    $rootScope.$on('$stateChangeSuccess',
+      function (event, toState, toParams, fromState, fromParam) {
+    });
+
+    $rootScope.$on('$stateChangeError',
+      function (event, toState, toParams, fromState, fromParam) {
+    });
+  }
+
+
+})();
